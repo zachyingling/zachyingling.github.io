4b9029d8fef5f164776693b7abc6766ae5ed2810
Zach Yingling <zpyingling9559@gmail.com>
2020-07-06 19:46:02 -0400

    trying to learn php and to get my contact form to work using php

---
+++ contactform.php
@@ -0,0 +1,17 @@
+<?php 
+
+if (isset($_POST['submit'])) {
+  $name = $_POST['name'];
+  $surname = $_POST['surname'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $mailTo = "zpyingling9559@gmail.com"
+  $headers = "From: ".$email;
+  $txt = "You have received an e-mail from ".$name." ".$surname.".\n\n".$message;
+
+  mail($mailTo, "No Subject", $txt, $headers);
+  header("Location: index.html?mailsend");
+}
+
+?>
