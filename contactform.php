db0018ff0c9a9fa00f3255a18c581df2256e6b63
Zach Yingling <zpyingling9559@gmail.com>
2020-07-06 20:38:43 -0400

    forgetting semicolon on a line; might have fixed the deployed version

--- contactform.php
+++ contactform.php
@@ -6,12 +6,12 @@ if (isset($_POST['submit'])) {
   $email = $_POST['email'];
   $message = $_POST['message'];
   
-  $mailTo = "zpyingling9559@protonmail.com"
-  $headers = "From: ".$email;
-  $txt = "You have received an e-mail from ".$name." ".$surname.".\n\n".$message;
+  $mailTo = 'zpyingling9559@protonmail.com';
+  $headers = 'From: '.$email;
+  $txt = 'You have received an e-mail from '.$name.' '.$surname.'.\n\n'.$message;
 
-  mail($mailTo, "No Subject", $txt, $headers);
-  header("Location: index.php?mailsend");
+  mail($mailTo, 'No Subject', $txt, $headers);
+  header('Location: index.php?mailsend');
 }
 
 ?>
