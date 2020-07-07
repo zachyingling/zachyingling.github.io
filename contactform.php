e0231a9898d9ac003f10b9224f347289b24872d5
Zach Yingling <zpyingling9559@gmail.com>
2020-07-06 23:03:13 -0400

    changing from mail() function to PHPMailer to send mail from a Smtp server

--- contactform.php
+++ contactform.php
@@ -1,17 +1,47 @@
-<?php 
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
 
 if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $surname = $_POST['surname'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  
-  $mailTo = 'zpyingling9559@protonmail.com';
-  $headers = 'From: '.$email;
-  $txt = 'You have received an e-mail from '.$name.' '.$surname.'.\n\n'.$message;
+  try {
+      $name = $_POST['name'];
+      $surname = $_POST['surname'];
+      $fullname = $name.' '.$surname;
+      $email = $_POST['email'];
+      $message = $_POST['message'];
 
-  mail($mailTo, 'No Subject', $txt, $headers);
-  header('Location: index.php?mailsend');
+      //Server settings
+      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+      $mail->isSMTP();                                            // Send using SMTP
+      $mail->Host       = 'smtp-relay.sendinblue.com';                    // Set the SMTP server to send through
+      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+      $mail->Username   = 'zpyingling9559@gmail.com';                     // SMTP username
+      $mail->Password   = 'hSUcJgdAjvkbMX3y';                               // SMTP password
+      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+      $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+      //Recipients
+      $mail->setFrom('zpyingling9559@gmail.com', 'Zachary');
+      $mail->addAddress($email, $fullname);     // Add a recipient
+
+      // Content
+      $mail->isHTML(true);                                  // Set email format to HTML
+      $mail->Subject = 'User email';
+      $mail->Body = $message;
+      $mail->AltBody = $message;
+
+      $mail->send();
+      echo 'Message has been sent';
+  } catch (Exception $e) {
+      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+  }
 }
-
-?>
