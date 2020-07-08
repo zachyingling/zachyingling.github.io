00ab6cf19931867013a13514a5b453fa6ca97ae4
Zach Yingling <zpyingling9559@gmail.com>
2020-07-08 16:31:32 -0400

    trying  rather than

--- contactform.php
+++ contactform.php
@@ -6,10 +6,10 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
-require_once 'vendorFolder/autoload.php';
+require 'vendorFolder/autoload.php';
 
 // Loading dotenv to create environment variables
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__. '/../');
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 $dotenv->required('GOOGLE_PASS')->notEmpty();
 
@@ -31,7 +31,7 @@ if (isset($_POST['submit'])) {
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
     $mail->SMTPSecure = 'tls';
     $mail->Username   = 'zachyingling9559@gmail.com';                     // SMTP username
-    $mail->Password   = $_ENV['GOOGLE_PASS'];                               // SMTP password
+    $mail->Password   = $_SERVER['GOOGLE_PASS'];                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
