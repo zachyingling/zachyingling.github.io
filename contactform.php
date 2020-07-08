e1a1cd2545a4a36468029dd5a70b26216e6acdf2
Zach Yingling <zpyingling9559@gmail.com>
2020-07-08 16:16:29 -0400

    still trying to get environment variable to work in php

--- contactform.php
+++ contactform.php
@@ -9,7 +9,7 @@ use PHPMailer\PHPMailer\Exception;
 require 'vendorFolder/autoload.php';
 
 // Loading dotenv to create environment variables
-$dotenv = \Dotenv\Dotenv::createImmutable('/..'.__DIR__ );
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ );
 $dotenv->load();
 $dotenv->required('GOOGLE_PASS')->notEmpty();
 
@@ -18,35 +18,35 @@ $mail = new PHPMailer(true);
 
 if (isset($_POST['submit'])) {
   try {
-      $name = $_POST['name'];
-      $surname = $_POST['surname'];
-      $fullname = $name.' '.$surname;
-      $email = $_POST['email'];
-      $message = $_POST['message'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $fullname = $name.' '.$surname;
+    $email = $_POST['email'];
+    $message = $_POST['message'];
 
-      //Server settings
-      $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-      $mail->isSMTP();                                            // Send using SMTP
-      $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-      $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-      $mail->SMTPSecure = 'tls';
-      $mail->Username   = 'zachyingling9559@gmail.com';                     // SMTP username
-      $mail->Password   = $_ENV['GOOGLE_PASS'];                               // SMTP password
-      $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-      $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->SMTPSecure = 'tls';
+    $mail->Username   = 'zachyingling9559@gmail.com';                     // SMTP username
+    $mail->Password   = $_ENV['GOOGLE_PASS'];                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
-      //Recipients
-      $mail->setFrom($email, $fullname);
-      $mail->addAddress('zachyingling9559@gmail.com');     // Add a recipient
+    //Recipients
+    $mail->setFrom($email, $fullname);
+    $mail->addAddress('zachyingling9559@gmail.com');     // Add a recipient
 
-      // Content
-      $mail->isHTML(true);                                  // Set email format to HTML
-      $mail->Subject = $fullname;
-      $mail->Body = 'From: '.$email.'<br/><br/>'.$message;
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $fullname;
+    $mail->Body = 'From: '.$email.'<br/><br/>'.$message;
 
-      $mail->send();
-      echo 'Message has been sent';
+    $mail->send();
+    echo 'Message has been sent';
   } catch (Exception $e) {
-      echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
   }
 }
