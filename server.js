8ad3d58f538b711f5a3c891b3d7ab0945457cc85
Zach Yingling <zpyingling9559@gmail.com>
2020-07-08 20:13:47 -0400

    adding a redirect route whenever the email is sent and putting all static files in public folder so it can be used for the front end

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
+require("dotenv").config();
 const express = require("express");
-
 const app = express();
-
+const path = require("path");
 const PORT = process.env.PORT || 8080;
+
+const nodemailer = require("nodemailer");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/redirect", (req, res) => {
+  res.sendFile(path.join(__dirname, "redirect.html"));
+})
+
+app.post("/api/contact-form", (req, res) => {
+  let email = req.body.email;
+  let message = req.body.message;
+  let firstName = req.body.name;
+  let lastName = req.body.surname;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GOOGLE_EMAIL,
+      pass: process.env.GOOGLE_PASS
+    }
+  });
+
+  let mailOptions = {
+    from: email,
+    to: process.env.GOOGLE_EMAIL,
+    subject: "testing email",
+    text: message
+  };
+
+  transporter.sendMail(mailOptions, (err, data) => {
+    if (err) {
+      res.send("Error: " + err);
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 app.listen(PORT, function() {
